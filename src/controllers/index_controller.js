b26236efd2a6857dc76cdf106ccbf51c13b4058b
Jimmy Mora <46798314+jimovi94@users.noreply.github.com>
2021-11-09 23:57:19 -0500

    Fixes

--- src/controllers/index_controller.js
+++ src/controllers/index_controller.js
@@ -3,6 +3,7 @@ export default class extends Controller {
     contentful_api.getEntries({
       content_type: 'proyecto'
     }).then(function (entries) {
+      $("#index-proyectos").html("");
       entries.items.forEach(function (entry) {
         $("#index-proyectos").append(` 
         <div class="item">
